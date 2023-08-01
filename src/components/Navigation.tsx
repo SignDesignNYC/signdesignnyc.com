6434a5fc0009f63e00d112a0898fdc6d7031cbd9
Ricky Chon <rickychon99@gmail.com>
2023-07-31 23:08:20 -0400

    Add basic site layout

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,40 +1,48 @@
 export default function Navigation(): React.ReactNode {
   return (
-    <nav className="w-72 bg-zinc-900 p-4">
+    <nav className="sticky top-0 left-0 w-72 bg-zinc-900 py-6">
       <ul className="w-full grid gap-6 place-items-center">
-        <NavLink href="/awnings">
+        <NavLink
+          href="#awnings"
+          className="rounded-l-xl before:shadow-red-500 after:shadow-red-500"
+        >
           <span className="text-3xl font-bold text-white underline">Awnings</span>
         </NavLink>
-        <NavLink href="/neon">
+        <NavLink
+          href="#neon"
+          className="rounded-l-xl"
+        >
           <span className="text-3xl font-bold text-white underline">Neon</span>
         </NavLink>
-        <NavLink href="/channel-letters">
+        <NavLink href="#channel-letters">
           <span className="text-3xl font-bold text-white underline">Channel Letters</span>
         </NavLink>
-        <NavLink href="/pvc">
+        <NavLink href="#pvc">
           <span className="text-3xl font-bold text-white underline">PVC</span>
         </NavLink>
-        <NavLink href="/truck-stickers">
+        <NavLink href="#truck-stickers">
           <span className="text-3xl font-bold text-white underline">Truck Stickers</span>
         </NavLink>
-        <NavLink href="/stickers">
+        <NavLink href="#stickers">
           <span className="text-3xl font-bold text-white underline">Stickers</span>
         </NavLink>
       </ul>
     </nav>
-  )
+  );
 }
 
 function NavLink({
   href,
   children,
+  className = ""
 }: {
   href: string;
   children: React.ReactNode;
+  className?: string;
 }) {
   return (
-    <a href={href} target="_self">
-      <li className="py-3 px-4">
+    <a href={href} target="_self" className={`w-full text-center ${className}`}>
+      <li className="py-3 px-4 grid place-items-center">
         {children}
       </li>
     </a>
