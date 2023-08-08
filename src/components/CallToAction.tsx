892466c5220520908a77e59d37d40119a63cbbf0
Ricky Chon <rickychon99@gmail.com>
2023-08-08 08:45:23 -0400

    Extract call to action into own component

---
+++ src/components/CallToAction.tsx
@@ -0,0 +1,20 @@
+import type React from "react";
+
+export default function CallToAction(): React.ReactNode {
+  return (
+    <section className="mt-8 px-4 w-full grid">
+      <div className="place-self-center grid gap-6 w-full max-w-sm sm:max-w-md">
+        <h1 className="font-bold text-zinc-100 text-4xl">
+          Custom Vinyl Stickers, Awnings, Neon Signs, PVC, and more!
+        </h1>
+        <a
+          href="mailto:signdesignnyc@gmail.com"
+          target="_blank"
+          className="grid place-items-center rounded-full p-2 bg-yellow-400 hover:cursor-pointer"
+        >
+          <span className="font-bold text-zinc-800 text-2xl">Contact for Pricing</span>
+        </a>
+      </div>
+    </section>
+  );
+}
