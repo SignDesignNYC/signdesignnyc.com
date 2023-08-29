36eb94b44b6d4564b34c34ed1a99e16fdb056836
Ricky Chon <rickychon99@gmail.com>
2023-08-29 15:42:42 -0400

    Display product list in accordion

--- src/components/ProductShowcase.tsx
+++ src/components/ProductShowcase.tsx
@@ -1,4 +1,10 @@
 import type React from "react";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 
 export default function ProductShowcase(): React.ReactNode {
   return (
@@ -7,7 +13,17 @@ export default function ProductShowcase(): React.ReactNode {
         place-self-center grid gap-6 w-full max-w-sm sm:max-w-lg md:max-w-2xl lg:max-w-5xl
       ">
         <h1 className="font-bold text-4xl text-zinc-100">Products</h1>
-        <ProductList />
+        <Accordion type="single" collapsible className="w-full">
+          {productList.map((product, idx) => (
+            <AccordionItem value={product.id} key={idx} className="border-b-zinc-700">
+              <AccordionTrigger className="text-zinc-100 text-xl">{product.title}</AccordionTrigger>
+              <AccordionContent className="text-zinc-100">
+                ({product.images.length}) Images of {product.title}
+              </AccordionContent>
+            </AccordionItem>
+          ))}
+        </Accordion>
+
         {/* TODO: Progressive low-quality images for fast loading */}
         {/* TODO: Click image to show HD quality */}
         {products.map((product, idx) => (
@@ -28,52 +44,6 @@ export default function ProductShowcase(): React.ReactNode {
         ))}
       </div>
     </section>
-  );
-}
-
-function ProductList(): React.ReactNode {
-  return (
-   <ul className="grid">
-      <ProductListItem title="A-Frame & Stand Sign" />
-      <ProductListItem title="Aluminum Box & Panel Sign" />
-      <ProductListItem title="Backlit Sign" />
-      <ProductListItem title="Banner Sign" />
-      <ProductListItem title="Bus, Truck, Van & Door Lettering" />
-      <ProductListItem title="Cafe Stand & Stand Sign" />
-      <ProductListItem title="Cast Metal Letters" />
-      <ProductListItem title="Channel Letters" />
-      <ProductListItem title="Corrugated Board Sign" />
-      <ProductListItem title="Cut PVC 3D Letters" />
-      <ProductListItem title="Directory Board Sign" />
-      <ProductListItem title="Embossed Letters" />
-      <ProductListItem title="Engraving Wood Sign" />
-      <ProductListItem title="Fabric Awning" />
-      <ProductListItem title="Fabric Flag Sign" />
-      <ProductListItem title="Light Box" />
-      <ProductListItem title="Neon Sign" />
-      <ProductListItem title="Plastic Panel Sign" />
-      <ProductListItem title="PVC Board" />
-      <ProductListItem title="Retractable Awning" />
-      <ProductListItem title="Room Number Sign" />
-      <ProductListItem title="Stand-Off Screws Sign" />
-      <ProductListItem title="Sticker Sign" />
-      <ProductListItem title="Vestibule Panel" />
-      <ProductListItem title="Windmaster Sign" />
-    </ul>
-  );
-}
-
-function ProductListItem({
-  title,
-}: {
-  title: string;
-}): React.ReactNode {
-  return (
-    <li>
-      <button className="w-full h-full hover:bg-zinc-300/20 p-4 border border-zinc-300/20">
-        <span className="font-bold text-zinc-100 text-3xl">{title}</span>
-      </button>
-    </li>
   );
 }
 
@@ -106,6 +76,134 @@ function ProductImage({
     />
   );
 }
+
+const productList = [
+  {
+    id: "a-frame-stand-sign",
+    title: "A-Frame & Stand Sign",
+    images: [],
+  },
+  {
+    id: "aluminum-box-panel-sign",
+    title: "Aluminum Box & Panel Sign",
+    images: [],
+  },
+  {
+    id: "backlit-sign",
+    title: "Backlit Sign",
+    images: [],
+  },
+  {
+    id: "banner-sign",
+    title: "Banner Sign",
+    images: [],
+  },
+  {
+    id: "bus-truck-van-door-lettering",
+    title: "Bus, Truck, Van & Door Lettering",
+    images: [],
+  },
+  {
+    id: "cafe-stand-sign",
+    title: "Cafe Stand & Stand Sign",
+    images: [],
+  },
+  {
+    id: "cast-metal-letters",
+    title: "Cast Metal Letters",
+    images: [],
+  },
+  {
+    id: "channel-letters",
+    title: "Channel Letters",
+    images: [],
+  },
+  {
+    id: "corrugated-board-sign",
+    title: "Corrugated Board Sign",
+    images: [],
+  },
+  {
+    id: "cut-pvc-3d-letters",
+    title: "Cut PVC 3D Letters",
+    images: [],
+  },
+  {
+    id: "directory-board-sign",
+    title: "Directory Board Sign",
+    images: [],
+  },
+  {
+    id: "embossed-letters",
+    title: "Embossed Letters",
+    images: [],
+  },
+  {
+    id: "engraving-wood-sign",
+    title: "Engraving Wood Sign",
+    images: [],
+  },
+  {
+    id: "fabric-awning",
+    title: "Fabric Awning",
+    images: [],
+  },
+  {
+    id: "fabric-flag-sign",
+    title: "Fabric Flag Sign",
+    images: [],
+  },
+  {
+    id: "light-box",
+    title: "Light Box",
+    images: [],
+  },
+  {
+    id: "neon-sign",
+    title: "Neon Sign",
+    images: [],
+  },
+  {
+    id: "plastic-panel-sign",
+    title: "Plastic Panel Sign",
+    images: [],
+  },
+  {
+    id: "pvc-board",
+    title: "PVC Board",
+    images: [],
+  },
+  {
+    id: "retractable-awning",
+    title: "Retractable Awning",
+    images: [],
+  },
+  {
+    id: "room-number-sign",
+    title: "Room Number Sign",
+    images: [],
+  },
+  {
+    id: "stand-off-screws-sign",
+    title: "Stand-Off Screws Sign",
+    images: [],
+  },
+  {
+    id: "sticker-sign",
+    title: "Sticker Sign",
+    images: [],
+  },
+  {
+    id: "vestibule-panel",
+    title: "Vestibule Panel",
+    images: [],
+  },
+  {
+    id: "windmaster-sign",
+    title: "Windmaster Sign",
+    images: [],
+  },
+];
 
 const products = [
   {
